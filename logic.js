6ac61e8624cd27c336a3f6b5d11178d139711d01
BL1NK813 <ya.alexda9219@gmail.com>
2024-06-16 03:23:02 +0500

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,8 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-     setTimeout(function() {
-        document.getElementById('loading-screen').style.display = 'none';
+    const dotsElement = document.getElementById('dots');
+    let dotCount = 0;
+
+    const changeDots = () => {
+        dotCount = (dotCount + 1) % 4;
+        dotsElement.textContent = '.'.repeat(dotCount);
+    };
+
+    const intervalId = setInterval(changeDots, 500);
+
+    setTimeout(function() {
+        clearInterval(intervalId);
+        document.getElementById('progress_few').style.display = 'none';
         document.querySelector('.container').style.display = 'block';
-     }, 4000);
+    }, 3000);
 });
 
 let energyTap = 1000;
