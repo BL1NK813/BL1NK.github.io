0cc03ab6f3cb4eb9949780ba16c921086f7033cd
BL1NK813 <ya.alexda9219@gmail.com>
2024-06-28 22:07:09 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -107,24 +107,4 @@ document.addEventListener('DOMContentLoaded', function() {
         const farmCoinsElement = document.getElementById('farm_coins_id');
         farmCoinsElement.innerHTML = `<img src="dicpic/coin.png" alt="coin">${parseInt(farmCoinsElement.textContent) || 0}`;
     }
-
-    const barButtons = document.querySelectorAll('.bar-button');
-    const barContents = document.querySelectorAll('.bar-content');
-
-    barButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            const targetTab = button.getAttribute('data-tab');
-
-            barButtons.forEach(btn => btn.classList.remove('active'));
-            barContents.forEach(content => content.classList.remove('active'));
-
-            // Добавляем класс active к текущей кнопке и контенту
-            button.classList.add('active');
-            document.getElementById(targetTab).classList.add('active');
-        });
-    });
-
-    // Устанавливаем активную первую вкладку по умолчанию
-    barButtons[0].classList.add('active');
-    barContents[0].classList.add('active');
 });
